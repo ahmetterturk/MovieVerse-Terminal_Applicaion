0ac5215b9b42dc3125d04d70b8ee07c2ea18106f
ahmetterturk <ahm.ert@hotmail.com>
2021-04-05 17:09:26 +1000

    create movies.rb and populate some movies

--- src/Movie.rb
+++ src/Movie.rb
@@ -1,11 +1,25 @@
 class Movie 
     attr_accessor :title, :year, :price
 
+    MOVIES = []
+
     def initialize(title, year, price)
         @title = title
         @year = year 
         @price = price
+        MOVIES << self
     end
+
+    def self.display_movies
+        MOVIES
+    end
+
+    def to_s
+        "Title: #{@title} \nYear: #{@year}  \nPrice: #{@price}"
+    end
+
+
+
 
 
 end
