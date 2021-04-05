11073c2abe6fd3a7e39e5ebd2df48aa1beabcc49
ahmetterturk <ahm.ert@hotmail.com>
2021-04-05 17:05:24 +1000

    initial comit, create Movie.rb file

---
+++ src/Movie.rb
@@ -0,0 +1,11 @@
+class Movie 
+    attr_accessor :title, :year, :price
+
+    def initialize(title, year, price)
+        @title = title
+        @year = year 
+        @price = price
+    end
+
+
+end
