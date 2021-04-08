3ddea2e7738b06dd17f4a82b7a2692282ab4b024
ahmetterturk <ahm.ert@hotmail.com>
2021-04-08 13:22:17 +1000

    install pastel gem and apply it to banner on movies.rb

--- src/movies.rb
+++ src/movies.rb
@@ -1,10 +1,12 @@
 require_relative './Movie.rb'
 require_relative './MovieController'
+require 'pastel'
 
 django = Movie.new('Django Unchained', '2015', 30)
 hobbit = Movie.new('The Hobbit', '2000', 20)
 harry_potter = Movie.new('harry Potter', '2001', 40)
 
+pastel = Pastel.new
 banner = """
 
                                  ,MMM8&&&.
@@ -23,12 +25,13 @@ banner = """
 
 """
 
-puts banner
+puts pastel.blue(banner)
+puts 
 puts "Welcome to MovieVerse"
 
 begin
    puts "What would you like to do?"
-   puts "Buy\nSell\nView Movies\nShow Movie\nExit"
+   puts "Buy Movies\nSell Movies\nView Movies\nCheckout Movie\nExit"
    input, id = gets.chomp.downcase.split(' ')
 
    if input == 'buy'
