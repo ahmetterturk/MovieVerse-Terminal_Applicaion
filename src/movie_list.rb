12a7bd2c5f479c9218700b839c7742cc89c30191
ahmetterturk <ahm.ert@hotmail.com>
2021-04-09 09:59:00 +1000

    create a movie list file which stores some movie data

---
+++ src/movie_list.rb
@@ -0,0 +1,13 @@
+module MovieList 
+    def self.movies
+        movie_list = [
+            {title: "Harry Potter", year:2013 , price: 30 },
+            {title: "Django Unchained", year: 2015, price: 25},
+            {title: "The Wolf Of Wall Street", year: 2013 , price: 23},
+            {title: "The Hobbit 1", year: 2013 , price: 12},
+            {title: "The Hobbit 2", year: 2014, price: 22},
+            {title: "Are We There Yet?", year: 2006, price: 10 },
+            {title: "The Da Vinci Code", year: 2012, price: 14}
+        ]
+    end
+end
