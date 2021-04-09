6e6cea6a6cc75caa80ef2c45397e7e6eb1a10b5d
ahmetterturk <ahm.ert@hotmail.com>
2021-04-09 16:56:40 +1000

    save 20 movies to list for opening

--- src/movie_list.rb
+++ src/movie_list.rb
@@ -1,13 +1,101 @@
-module MovieList 
-    def self.movies
-        movie_list = [
-            {title: "Harry Potter", year:2013 , price: 30 },
-            {title: "Django Unchained", year: 2015, price: 25},
-            {title: "The Wolf Of Wall Street", year: 2013 , price: 23},
-            {title: "The Hobbit 1", year: 2013 , price: 12},
-            {title: "The Hobbit 2", year: 2014, price: 22},
-            {title: "Are We There Yet?", year: 2006, price: 10 },
-            {title: "The Da Vinci Code", year: 2012, price: 14}
-        ]
-    end
-end
+# ---
+# - !ruby/object:Movie
+#   id: 1
+#   title: Django Unchained
+#   year: 2012
+#   price: 25
+# - !ruby/object:Movie
+#   id: 2
+#   title: Harry Potter and the Sorcerer's Stone
+#   year: 2001
+#   price: 18
+# - !ruby/object:Movie
+#   id: 3
+#   title: Harry Potter And The Chamber Of Secrets
+#   year: 2002
+#   price: 18
+# - !ruby/object:Movie
+#   id: 4
+#   title: Snatch
+#   year: 2000
+#   price: 15
+# - !ruby/object:Movie
+#   id: 5
+#   title: The Wolf of Wall Street
+#   year: 2000
+#   price: 25
+# - !ruby/object:Movie
+#   id: 6
+#   title: The Avengers 
+#   year: 2012
+#   price: 22
+# - !ruby/object:Movie
+#   id: 7
+#   title: Thor
+#   year: 2011
+#   price: 21
+# - !ruby/object:Movie
+#   id: 8
+#   title: Spider-Man
+#   year: 2002
+#   price: 15
+# - !ruby/object:Movie
+#   id: 9
+#   title: The Shawshank Redemption
+#   year: 1994
+#   price: 13
+# - !ruby/object:Movie
+#   id: 10
+#   title: The Dark Night
+#   year: 2008
+#   price: 24
+# - !ruby/object:Movie
+#   id: 11
+#   title: The Lord of the Rings; Fellowship of the Ring 
+#   year: 2001
+#   price: 20
+# - !ruby/object:Movie
+#   id: 12
+#   title: The Godfather
+#   year: 1972
+#   price: 25
+# - !ruby/object:Movie
+#   id: 13
+#   title: The Godfather; Part 2
+#   year: 1974
+#   price: 25
+# - !ruby/object:Movie
+#   id: 14
+#   title: Pulp Fiction 
+#   year: 1994
+#   price: 28
+# - !ruby/object:Movie
+#   id: 15
+#   title: Fight Club
+#   year: 1999
+#   price: 22
+# - !ruby/object:Movie
+#   id: 16
+#   title: The Good, the Bad and the Ugly
+#   year: 1966
+#   price: 15
+# - !ruby/object:Movie
+#   id: 17
+#   title: Forrest Gump 
+#   year: 1994
+#   price: 17
+# - !ruby/object:Movie
+#   id: 18
+#   title: Inception 
+#   year: 2010
+#   price: 21
+# - !ruby/object:Movie
+#   id: 19
+#   title: Interstellar
+#   year: 2014
+#   price: 16
+# - !ruby/object:Movie
+#   id: 20
+#   title: Gladiator
+#   year: 2000
+#   price: 20
