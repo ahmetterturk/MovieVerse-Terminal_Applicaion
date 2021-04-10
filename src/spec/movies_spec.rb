d97bfb9b5be4a74dbfab4c7841b15d5896c8fa21
ahmetterturk <ahm.ert@hotmail.com>
2021-04-10 11:11:41 +1000

    add new test on save method

--- src/spec/movies_spec.rb
+++ src/spec/movies_spec.rb
@@ -1,15 +1,12 @@
 require_relative '../Movie'
 require_relative '../MovieController'
 
-
 RSpec.describe Movie.new("Shrek", 3, 15) do
     it { is_expected.to have_attributes(:title => "Shrek") }
     it { is_expected.to have_attributes(:title => a_string_starting_with("S") ) }
     it { is_expected.to have_attributes(:year => 3) }
     it { is_expected.to have_attributes(:price => (a_value > 1) ) }
 end
-
-
 
 RSpec.describe Movie do
 
@@ -26,6 +23,13 @@ RSpec.describe Movie do
 
         it "should respond to price" do 
             expect(subject).to respond_to :price 
+        end
+    end
+
+    describe "save" do
+        it "should increase movie file size" do
+            movie = Movie.new("Shrek", 2000, 20)
+            expect { movie.save }.to change(Movie::MOVIES, :size).by(1) 
         end
     end
 
